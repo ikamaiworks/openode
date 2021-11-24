96c540a934c9dae9c66ea6c6f2f1ddab1cace9bf
ikamaiworks <90882722+ikamaiworks@users.noreply.github.com>
2021-11-24 17:21:49 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,19 @@
-import time
+from typing import Optional
+import uvicorn
+from fastapi import FastAPI
 
-while True:
-  print("crot")
-  time.sleep(2)
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+
+
+if __name__ == "__main__":
+    uvicorn.run("example:app", host="0.0.0.0", port=80, log_level="info")
